3052ff52f6c16dec373a90f4c10e4c5fbf797776
Diego Carmona <diegocarmona@my.whccd.edu>
2023-11-18 20:08:15 -0800

    added reverse function 1

--- script.js
+++ script.js
@@ -50,3 +50,12 @@ console.log(sum(range(1,10)));
 // let arrayValue = [1, 2, 3, 4, 5];
 // reverseArrayInPlace(arrayValue);
 // console.log(arrayValue); // outputs [5, 4, 3, 2, 1]
+function reverseArray(arr) {
+    var newArray = [];
+    for (var i = arr.length - 1; i >= 0; i--) {
+    newArray.push(arr[i]);
+}
+return newArray;
+}
+console.log(reverseArray(["A", "B", "C"]))
+

d010b8d7e59e343b5df49c31fd9610faf7d62361
Diego Carmona <diegocarmona@my.whccd.edu>
2023-11-18 19:50:16 -0800

    corrected improper syntax

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ function sum(numArray) {
    }
    return arrayTotal;
 }
+console.log(sum(range(1,10)));
 // 3. Arrays have a reverse method that changes the array by inverting the order in which
 // its elements appear. For this exercise, write two functions, reverseArray and
 // reverseArrayInPlace. The first, reverseArray, takes an array as an argument and
